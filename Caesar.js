12c1a71120b153c0743737a7743df6450e81fe12
Robert Westbrook <robert_westbrook@alumni.baylor.edu>
2020-07-16 16:05:56 -0400

    Update Caesar.js
    Added description of the code.
    Todo: add an alternative code to the python file that follows the logic of the JS code using the % .

--- Caesar.js
+++ Caesar.js
@@ -9,10 +9,19 @@ function rot13(str) {
 }
 
 
-//Understnding the Code:
-//line 2 returns the string with a regex replace parameter that 
-//replaces any of the target characters with the result of the arrow
-//function =>
-//Line 3: calls the String.fromCharCode to take the character output from the
-//parameter and turns translates it into letter.
-//The Parameter L.charCodeAt(0) % 26) + 65
+//Understanding the Modulo:
+//This code takes the str and filters through it with regexp to pull out alpha and
+//then it checks the ascii character code to decide with the if statement what to
+//do with the charachter based on whether the character is lower case or upper.
+//Once it decides, the code then returns each character and replaces the original
+//using String.fromCharCode() function passing the argument L.charCodeAt(0). This
+//argument is simply getting the current index/letter and then it turns it into a ascii
+//code. Before that code is passed as an int to the .fromCharCode() function,
+//it takes the modulo % 26 of that letter's CharCode, adds the base ascii number of the
+//uppercase or lower case codes, before returning. The Modulo is basicallly dividing the
+//expressed number into the value to the left of it and returning its remainder. Once we 
+//get that remainder it is then what the base number from the section of ascii is being 
+//added to get the desired charCode before returning it to the .fromCharCode()function.
+//So, if "A" = 65 then % 26 of "A" == 13. Take 13 + 65 to equal your new character code
+//"N" = 78. If the code registers a lower case the previous modulo statement remains the
+//same but the base number added will change to 97 which is = "a" in ascii.
