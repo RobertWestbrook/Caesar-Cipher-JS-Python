823edeab7a7c4abea3638e3317061c92dc17d2dc
Robert Westbrook <robert_westbrook@alumni.baylor.edu>
2020-07-21 09:19:01 -0500

    Update Caesar.py
    Simplified code. The code now no longer needs to be decrypted only ran again with the encrypted message to be translated.

--- Caesar.py
+++ Caesar.py
@@ -1,20 +1,9 @@
-import re
 def rot13(mess):
-    '''Takes a string a turns it into a rot13 cipher encrypted string'''
-    In ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    Out = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+    In = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    Out = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
     rot = str.maketrans(In,Out)
-    message = mess
-    print (message.translate(rot))
-
-
-def deRot13(mess):
-    '''Takes a rot13 message/string and decrypts it.'''
-    In ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    Out = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
-    rot = str.maketrans(Out,In)
-    message = mess
-    print (message.translate(rot))
-
+    translation = mess.translate(rot)
+    print (translation)
+    return translation
 
 
